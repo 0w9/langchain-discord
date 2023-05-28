fbf8edcf75a6b2512003f0abde217a6cbf93eb74
Restyled.io <commits@restyled.io>
2023-05-28 12:57:56 +0000

    Restyled by autopep8

--- langchain_discord/webhook.py
+++ langchain_discord/webhook.py
@@ -2,14 +2,15 @@ from typing import Optional
 from langchain.tools import BaseTool
 from pydantic import BaseModel, Field
 from typing import Optional, Type
-import requests, os
+import requests
+import os
 from typing import Any, Dict
 from pydantic import BaseModel, Field, root_validator
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
-## INFO: The following code is not used anymore, but I keep it here for reference. It is good to keep for the future when more errors are handled by the package.
+# INFO: The following code is not used anymore, but I keep it here for reference. It is good to keep for the future when more errors are handled by the package.
 
-#class DiscordWebhookInput(BaseModel):
+# class DiscordWebhookInput(BaseModel):
 #    webhook_message: str = Field(description="The message you want to send to the webhook")
 #
 #    @root_validator
@@ -21,35 +22,38 @@ from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 #            raise ValueError("webhook_message must be a string")
 #
 #        return values
-        
+
 
 class DiscordWebhookTool(BaseTool):
     name = "discord_webhook"
     description = "useful for when you need to send a webhook message"
 
-    ## InFO: See above.
-    #args_schema: Type[DiscordWebhookInput] = DiscordWebhookInput
+    # InFO: See above.
+    # args_schema: Type[DiscordWebhookInput] = DiscordWebhookInput
 
     def _run(self, webhook_message: str) -> str:
-        if(os.environ.get("WEBHOOK_URL") is None): raise ValueError("WEBHOOK_URL environment variable not set")
-        if(os.environ.get("WEBHOOK_URL") is None): raise ValueError("WEBHOOK_URL environment variable not set")
-        
+        if (os.environ.get("WEBHOOK_URL") is None):
+            raise ValueError("WEBHOOK_URL environment variable not set")
+        if (os.environ.get("WEBHOOK_URL") is None):
+            raise ValueError("WEBHOOK_URL environment variable not set")
+
         WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
-        WEBHOOK_USERNAME = os.environ.get("WEBHOOK_USERNAME", "LangChain Agent")
+        WEBHOOK_USERNAME = os.environ.get(
+            "WEBHOOK_USERNAME", "LangChain Agent")
 
-        answer = requests.post(WEBHOOK_URL, 
-                      json={
-                            "content": webhook_message, 
-                            "username": WEBHOOK_USERNAME
-                        }, 
-                      headers={"Content-Type": "application/json"}
-        )
+        answer = requests.post(WEBHOOK_URL,
+                               json={
+                                   "content": webhook_message,
+                                   "username": WEBHOOK_USERNAME
+                               },
+                               headers={"Content-Type": "application/json"}
+                               )
 
-        if(answer.status_code != 204 and answer.status_code != 200):
-            raise Exception("Webhook message could not be sent! Please check your webhook URL and message and try again.")
+        if (answer.status_code != 204 and answer.status_code != 200):
+            raise Exception(
+                "Webhook message could not be sent! Please check your webhook URL and message and try again.")
         else:
             return "Webhook message sent successfully!"
 
-    
-    def _arun(self, webhook_message: str, webhook_url: str, webhook_username: str): raise NotImplementedError("This tool does not support async")
-
+    def _arun(self, webhook_message: str, webhook_url: str,
+              webhook_username: str): raise NotImplementedError("This tool does not support async")
